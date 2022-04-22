c5fbc33ba7c1e65b11b2f43cb6c7b7b784ebbfb7
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-04-22 09:19:26 +0530

    adding the first branch

--- lib/main.dart
+++ lib/main.dart
@@ -2,4 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'view/expense_checker_app.dart';
 
+/// adding first branch
+/// ignoring the pull request
+///
 void main() => runApp(const ExpenseCheckerApp());
